e8b0f1a015483077a413128b4fc4e1017593ed3e
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-06-10 00:32:24 +0900

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,22 @@
 import { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixed_number: 0,
+  };
   render() {
-    const { number } = this.state;
+    const { number, fixed_number } = this.state;
     return (
       <div>
-        <h1 className='react2'>{number}</h1>
-        <button className='react' onClick={() => this.setState({ number: number + 1 })}>
+        <h1 className="react2">증가하는 값 : {number}</h1>
+        <h2 className="react2">고정된 값 : {fixed_number}</h2>
+        <button
+          className="react"
+          onClick={() => {
+            this.setState({ number: number + 1 }, () => {console.log("calling setState"); console.log(this.state)});
+          }}
+        >
           +1
         </button>
       </div>
