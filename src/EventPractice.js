2f4216542c504d4fe79b65c72eedad187b85b310
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-06-14 17:16:30 +0900

    practice

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,10 +1,38 @@
 import { Component } from "react";
 
 class EventPractice extends Component {
+  state = {
+    message: "",
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ message: e.target.value });
+  }
+
+  handleClick(e) {
+    alert(this.state.message);
+    this.setState({ message: "" });
+  }
+
   render() {
     return (
       <div>
         <h1>Event Practice</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력하세요."
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+
+        <button onClick={this.handleClick}>초기화</button>
       </div>
     );
   }
