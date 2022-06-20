24f40af1276c1f3b2919206c4b88a002698a0ccc
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-06-20 14:01:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import ScrollBox from "./ScrollBox";
-import { Component } from "react";
+import IterationSample from "./IterationSample";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ScrollBox ref = {(ref) => this.scrollBox=ref}/>
-        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <IterationSample/>
+    </div>
+  )
 }
 
 export default App;
